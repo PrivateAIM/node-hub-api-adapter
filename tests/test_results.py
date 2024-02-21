01c57f12a1c697f850ffc4eab61a6be1938fb743
Bruce Schultz <bschultz013@gmail.com>
2024-02-21 12:36:22 +0100

    test: add simple read_from_scratch unit test

--- tests/test_results.py
+++ tests/test_results.py
@@ -1,4 +1,6 @@
 """Test the results eps."""
+import uuid
+
 from starlette import status
 
 from tests.pseudo_auth import fakeauth
@@ -9,9 +11,8 @@ class TestResults:
 
     def test_read_from_scratch(self, test_client):
         """Test the read_from_scratch method."""
-        r = test_client.get("/scratch/{object_id}", auth=fakeauth)
+        r = test_client.get(f"/scratch/{uuid.uuid4()}", auth=fakeauth)
         assert r.status_code == status.HTTP_200_OK
 
-        namespace_list = r.json()
-        assert len(namespace_list)
-        assert isinstance(namespace_list, list)
+        upload = r.json()
+        assert len(upload)
