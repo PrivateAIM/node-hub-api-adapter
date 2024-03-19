e3896c320c62cc3c5373e4d40a4986f4c2a28f45
Bruce Schultz <bschultz013@gmail.com>
2024-03-19 13:05:58 +0100

    feat(server): add token endpoint

--- gateway/server.py
+++ gateway/server.py
@@ -1,12 +1,16 @@
 """Methods for verifying auth."""
+from typing import Annotated
 
+import requests
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, HTTPException
+from fastapi.security import OAuth2PasswordRequestForm
 from starlette import status
 from starlette.middleware.cors import CORSMiddleware
 
 from gateway.auth import realm_idp_settings
 from gateway.models import HealthCheck
+from gateway.models.conf import Token
 from gateway.routers.hub import hub_router
 from gateway.routers.k8s import k8s_router
 from gateway.routers.kong import kong_router
@@ -62,6 +66,33 @@ def get_health() -> HealthCheck:
         HealthCheck: Returns a JSON response with the health status
     """
     return HealthCheck(status="OK")
+
+
+@app.post(
+    "/token",
+    summary="Get a token from the IDP",
+    status_code=status.HTTP_200_OK,
+    response_model=Token,
+)
+def get_token(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]) -> Token:
+    """Get a token from the IDP."""
+    payload = {
+        "username": form_data.username,
+        "password": form_data.password,
+        "client_id": realm_idp_settings.client_id,
+        "client_secret": realm_idp_settings.client_secret,
+        "grant_type": "password",
+        "scope": "openid",
+    }
+    resp = requests.post(realm_idp_settings.token_url, data=payload)
+    if not resp.ok:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail=resp.json(),  # Invalid authentication credentials
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    token_data = resp.json()
+    return Token(**token_data)
 
 
 app.include_router(
