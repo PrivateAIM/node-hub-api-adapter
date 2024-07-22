457ec53555e71824e7c1038f70775638be1c9fcd
Bruce Schultz <bschultz013@gmail.com>
2024-07-22 09:49:21 +0200

    docs(conf): update missing conf variable error message

--- hub_adapter/exceptions.py
+++ hub_adapter/exceptions.py
@@ -5,4 +5,4 @@ class ConfigError(Exception):
     """Raised when a config file is invalid."""
 
     def __init__(self, option: str, value: str):
-        super().__init__(f"Invalid value for {option}: {value}")
+        super().__init__(f"Check your config or env variables, invalid value for {option}: {value}")
