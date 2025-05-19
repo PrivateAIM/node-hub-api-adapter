9c90b68ec171d2e343bc33b4eab9fc64318cb985
Bruce Schultz <bschultz013@gmail.com>
2025-05-19 12:09:50 +0200

    fix(auth): proper response when bad issuer URL provided

--- hub_adapter/auth.py
+++ hub_adapter/auth.py
@@ -26,6 +26,7 @@ logger = logging.getLogger(__name__)
 @lru_cache(maxsize=2)
 def fetch_openid_config(oidc_url: str, max_retries: int = 6) -> OIDCConfiguration:
     """Fetch the openid configuration from the OIDC URL. Tries until it reaches max_retries."""
+    provided_url = oidc_url
     if not oidc_url.endswith(".well-known/openid-configuration"):
         oidc_url = oidc_url.rstrip("/") + "/.well-known/openid-configuration"
 
@@ -40,12 +41,22 @@ def fetch_openid_config(oidc_url: str, max_retries: int = 6) -> OIDCConfiguratio
         except httpx.ConnectError:  # OIDC Service not up yet
             attempt_num += 1
             wait_time = 10 * (2 ** (attempt_num - 1))  # 10s, 20s, 40s, 80s, 160s, 320s
-            logger.warning(f"Unable to contact the IDP at {oidc_url}, retrying in {wait_time} seconds")
+            logger.warning(
+                f"Unable to contact the IDP at {oidc_url}, retrying in {wait_time} seconds"
+            )
             time.sleep(wait_time)
 
-        except httpx.HTTPStatusError as e:
-            logger.error(f"HTTP error occurred while trying to contact the IDP: {e}")
-            raise httpx.HTTPStatusError from e
+        except httpx.HTTPStatusError:
+            err_msg = f"HTTP error occurred while trying to contact the IDP: {provided_url}, is this the correct issuer URL?"
+            logger.error(err_msg)
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail={
+                    "message": err_msg,
+                    "service": "Auth",
+                    "status_code": status.HTTP_404_NOT_FOUND,
+                },
+            )
 
     logger.error(f"Unable to contact the IDP at {oidc_url} after {max_retries} retries")
     raise httpx.ConnectError(f"Failed to connect after {max_retries} attempts.")
