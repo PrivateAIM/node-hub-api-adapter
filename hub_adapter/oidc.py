ecff59f5085992ab00289b5180315502c10a9a12
Bruce Schultz <bschultz013@gmail.com>
2025-08-15 14:00:44 +0200

    feat(auth): add internal token to PO requests

---
+++ hub_adapter/oidc.py
@@ -0,0 +1,83 @@
+"""Collection of methods related to gathering OIDC configurations."""
+import logging
+import time
+from functools import lru_cache
+
+import httpx
+from fastapi import HTTPException
+from starlette import status
+
+from hub_adapter.conf import hub_adapter_settings
+from hub_adapter.models.conf import OIDCConfiguration
+
+logger = logging.getLogger(__name__)
+
+
+@lru_cache(maxsize=2)
+def fetch_openid_config(
+    oidc_url: str,
+    max_retries: int = 6,
+) -> OIDCConfiguration:
+    """Fetch the openid configuration from the OIDC URL. Tries until it reaches max_retries."""
+    provided_url = oidc_url
+    if not oidc_url.endswith(".well-known/openid-configuration"):
+        oidc_url = oidc_url.rstrip("/") + "/.well-known/openid-configuration"
+
+    response = {}
+    attempt_num = 0
+    while attempt_num <= max_retries:
+        try:
+            response = httpx.get(oidc_url)
+
+            response.raise_for_status()
+            oidc_config = response.json()
+            return OIDCConfiguration(**oidc_config)
+
+        except (httpx.ConnectError, httpx.ReadTimeout):  # OIDC Service not up yet
+            attempt_num += 1
+            wait_time = 10 * (2 ** (attempt_num - 1))  # 10s, 20s, 40s, 80s, 160s, 320s
+            logger.warning(f"Unable to contact the IDP at {oidc_url}, retrying in {wait_time} seconds")
+            time.sleep(wait_time)
+
+        except httpx.HTTPStatusError as e:
+            err_msg = (
+                f"HTTP error occurred while trying to contact the IDP: {provided_url}, is this the correct issuer URL?"
+            )
+            logger.error(err_msg + f" - {e}")
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail={
+                    "message": err_msg,
+                    "service": "Auth",
+                    "status_code": status.HTTP_404_NOT_FOUND,
+                },
+            ) from e
+
+    logger.error(f"Unable to contact the IDP at {oidc_url} after {max_retries} retries")
+    raise httpx.ConnectError(f"Unable to contact the IDP at {oidc_url} after {max_retries} retries")
+
+
+def get_user_oidc_config() -> OIDCConfiguration:
+    """Lazy-load the user OIDC configuration when first needed."""
+    return fetch_openid_config(hub_adapter_settings.IDP_URL)
+
+
+def get_svc_oidc_config() -> OIDCConfiguration:
+    """Lazy-load the service OIDC configuration when first needed."""
+    # Services always use internal IDP
+    if hub_adapter_settings.NODE_SVC_OIDC_URL != hub_adapter_settings.IDP_URL:
+        return fetch_openid_config(hub_adapter_settings.NODE_SVC_OIDC_URL)
+    else:
+        return get_user_oidc_config()
+
+
+def check_oidc_configs_match() -> tuple[bool, OIDCConfiguration]:
+    """Check whether the user and svc OIDC configurations match and return the check (bool) and the svc OIDC
+    configuration if different otherwise the user configuration."""
+    user_oidc_config = get_user_oidc_config()
+    svc_oidc_config = get_svc_oidc_config()
+
+    if user_oidc_config.issuer != svc_oidc_config.issuer:
+        return False, svc_oidc_config
+    else:
+        return True, user_oidc_config
--- hub_adapter/routers/podorc.py
+++ hub_adapter/routers/podorc.py
@@ -10,6 +10,7 @@ from starlette.requests import Request
 from starlette.responses import Response
 
 from hub_adapter.auth import (
+    add_internal_token_if_missing,
     jwtbearer,
     verify_idp_token,
 )
@@ -25,10 +26,7 @@ from hub_adapter.models.podorc import (
 from hub_adapter.routers.hub import compile_analysis_pod_data
 
 po_router = APIRouter(
-    dependencies=[
-        Security(verify_idp_token),
-        Security(jwtbearer),
-    ],
+    dependencies=[Security(verify_idp_token), Security(jwtbearer), Depends(add_internal_token_if_missing)],
     tags=["PodOrc"],
     responses={404: {"description": "Not found"}},
 )
