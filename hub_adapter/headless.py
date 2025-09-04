c2977c528f2663dfa2ab48cd4346c071006948ab
Bruce Schultz <bschultz013@gmail.com>
2025-09-04 11:13:42 +0200

    refactor(headless): change to class

--- hub_adapter/headless.py
+++ hub_adapter/headless.py
@@ -10,15 +10,18 @@ from hub_adapter.auth import get_internal_token
 from hub_adapter.core import make_request
 from hub_adapter.dependencies import (
     compile_analysis_pod_data,
+    get_core_client,
+    get_flame_hub_auth_flow,
     get_node_id,
     get_node_metadata_for_url,
+    get_node_type_cache,
     get_registry_metadata_for_url,
     get_settings,
+    get_ssl_context,
 )
 from hub_adapter.oidc import check_oidc_configs_match
 from hub_adapter.routers.hub import (
     format_query_params,
-    get_node_type,
     list_analysis_nodes,
 )
 from hub_adapter.routers.kong import (
@@ -30,168 +33,189 @@ from hub_adapter.routers.kong import (
 logger = logging.getLogger(__name__)
 
 
-async def auto_start_analyses() -> set:
-    node_id = await get_node_id()
-    node_type = await get_node_type()
+class GoGoAnalysis:
+    def __init__(self):
+        self.settings = None
+        self.core_client = None
 
-    formatted_query_params = format_query_params({"sort": "-updated_at", "include": "analysis"})
-    analyses = await list_analysis_nodes(node_id=node_id, query_params=formatted_query_params)
-    valid_projects = await get_valid_projects()
-    ready_to_start_analyses = parse_analyses(analyses, valid_projects)
+        self.gather_deps()  # populates self.settings and self.core_client
 
-    analyses_started = set()
-    for analysis in ready_to_start_analyses:
-        analysis_id, project_id, node_id, _, _ = analysis
-        if node_type["type"] == "default":
-            kong_resp = await register_analysis(analysis_id, project_id)
-            if not kong_resp:
-                continue
+    def gather_deps(self):
+        """Gather all the dependencies needed to run the headless mode."""
+        settings = get_settings()
+        ssl_ctx = get_ssl_context(settings)
+        hub_robot = get_flame_hub_auth_flow(ssl_ctx, settings)
+        core_client = get_core_client(hub_robot, ssl_ctx, settings)
 
-            kong_token = kong_resp["keyauth"].key
+        self.settings = settings
+        self.core_client = core_client
 
-        else:  # Aggregator nodes don't need a kong store
-            kong_token = "none_needed"
+    async def auto_start_analyses(self) -> set:
+        """Gather and iterate over analyses from hub and start them if they pass checks."""
+        node_id = await get_node_id(core_client=self.core_client, hub_adapter_settings=self.settings)
+        node_type = await get_node_type_cache(hub_adapter_settings=self.settings, core_client=self.core_client)
 
-        props = {
-            "analysis_id": analysis_id,
-            "project_id": project_id,
-            "node_id": node_id,
-            "kong_token": kong_token,
-        }
-        start_resp, status_code = await start_analysis_pod(analysis_props=props, kong_token=kong_token)
-        if status_code == status.HTTP_201_CREATED:
-            analyses_started.add(analysis_id)
+        formatted_query_params = format_query_params({"sort": "-updated_at", "include": "analysis"})
+        analyses = await list_analysis_nodes(
+            core_client=self.core_client, node_id=node_id, query_params=formatted_query_params
+        )
+        valid_projects = await self.get_valid_projects()
+        ready_to_start_analyses = self.parse_analyses(analyses, valid_projects)
 
-    return analyses_started
+        analyses_started = set()
+        for analysis in ready_to_start_analyses:
+            analysis_id, project_id, node_id, _, _ = analysis
+            if node_type["type"] == "default":
+                kong_resp = await self.register_analysis(analysis_id, project_id)
+                if not kong_resp:
+                    continue
 
+                kong_token = kong_resp["keyauth"].key
 
-async def register_analysis(analysis_id: str, project_id: str, attempt: int = 1, max_attempts: int = 5) -> dict | None:
-    """Register an analysis with kong."""
-    logger.info(f"Attempt {attempt} at starting analysis {analysis_id}")
-    try:
-        kong_resp = await create_and_connect_analysis_to_project(project_id, analysis_id)
-        return kong_resp
+            else:  # Aggregator nodes don't need a kong store
+                kong_token = "none_needed"
 
-    except HTTPException as e:
-        if e.status_code == status.HTTP_404_NOT_FOUND:
-            logger.error(f"{e.detail['message']}, failed to start analysis {analysis_id}")
+            props = {
+                "analysis_id": analysis_id,
+                "project_id": project_id,
+                "node_id": node_id,
+                "kong_token": kong_token,
+            }
+            start_resp, status_code = await self.start_analysis_pod(analysis_props=props, kong_token=kong_token)
+            if status_code == status.HTTP_201_CREATED:
+                analyses_started.add(analysis_id)
 
-        elif e.status_code == status.HTTP_409_CONFLICT:
-            logger.warning(f"Analysis {analysis_id} already registered, checking if pod exists...")
-            pod_exists = await pod_running(analysis_id)
-            if pod_exists is None:  # Status could not be obtained, skip and try later
-                pass
+        return analyses_started
 
-            elif not pod_exists:  # Status obtained and if not running, delete kong consumer
-                logger.info(f"No pod found for {analysis_id}, will delete kong consumer and retry")
-                await delete_analysis(analysis_id=analysis_id)
+    async def register_analysis(
+        self, analysis_id: str, project_id: str, attempt: int = 1, max_attempts: int = 5
+    ) -> dict | None:
+        """Register an analysis with kong."""
+        logger.info(f"Attempt {attempt} at starting analysis {analysis_id}")
+        try:
+            kong_resp = await create_and_connect_analysis_to_project(project_id, analysis_id)
+            return kong_resp
 
-                if attempt < max_attempts:
-                    return await register_analysis(analysis_id, project_id, attempt + 1, max_attempts)
+        except HTTPException as e:
+            if e.status_code == status.HTTP_404_NOT_FOUND:
+                logger.error(f"{e.detail['message']}, failed to start analysis {analysis_id}")
+
+            elif e.status_code == status.HTTP_409_CONFLICT:
+                logger.warning(f"Analysis {analysis_id} already registered, checking if pod exists...")
+                pod_exists = await self.pod_running(analysis_id)
+                if pod_exists is None:  # Status could not be obtained, skip and try later
+                    pass
+
+                elif not pod_exists:  # Status obtained and if not running, delete kong consumer
+                    logger.info(f"No pod found for {analysis_id}, will delete kong consumer and retry")
+                    await delete_analysis(analysis_id=analysis_id)
+
+                    if attempt < max_attempts:
+                        return await self.register_analysis(analysis_id, project_id, attempt + 1, max_attempts)
+
+                    else:
+                        logger.error(f"Failed to start analysis {analysis_id} after {max_attempts} attempts")
 
                 else:
-                    logger.error(f"Failed to start analysis {analysis_id} after {max_attempts} attempts")
+                    logger.info(f"Pod already exists for analysis {analysis_id}, skipping start sequence")
 
             else:
-                logger.info(f"Pod already exists for analysis {analysis_id}, skipping start sequence")
+                logger.error(f"Failed to start analysis {analysis_id}, {e}")
 
-        else:
-            logger.error(f"Failed to start analysis {analysis_id}, {e}")
+    async def pod_running(self, analysis_id: str) -> bool | None:
+        """Check whether a pod with the given analysis_id is already running."""
+        pod_status = await self.fetch_analysis_status(analysis_id=analysis_id)
+        if pod_status:
+            return bool(pod_status["status"])  # If anything exists
 
+        return None  # Error occurred and no status retrieved
 
-async def pod_running(analysis_id: str) -> bool | None:
-    """Check whether a pod with the given analysis_id is already running."""
-    pod_status = await fetch_analysis_status(analysis_id=analysis_id)
-    if pod_status:
-        return bool(pod_status["status"])  # If anything exists
+    async def fetch_token_header(self) -> dict:
+        """Append OIDC token to headers."""
+        _, oidc_config = check_oidc_configs_match(self.settings)
+        token = await get_internal_token(oidc_config, self.settings)
+        return token
 
-    return None  # Error occurred and no status retrieved
+    async def start_analysis_pod(self, analysis_props: dict, kong_token: str) -> tuple[dict, int] | None:
+        """Start a new analysis pod."""
+        logger.info(f"Starting new analysis pod for {analysis_props['analysis_id']}")
 
-
-async def fetch_token_header() -> dict:
-    """Append OIDC token to headers."""
-    _, oidc_config = check_oidc_configs_match()
-    token = await get_internal_token(oidc_config)
-    return token
-
-
-async def start_analysis_pod(analysis_props: dict, kong_token: str) -> tuple[dict, int] | None:
-    """Start a new analysis pod."""
-    logger.info(f"Starting new analysis pod for {analysis_props['analysis_id']}")
-
-    node_metadata = get_node_metadata_for_url(analysis_props["node_id"])
-    analysis_info = get_registry_metadata_for_url(node_metadata)
-    props = compile_analysis_pod_data(
-        analysis_id=analysis_props["analysis_id"],
-        project_id=analysis_props["project_id"],
-        compiled_info=analysis_info,
-        kong_token=kong_token,
-    )
-
-    headers = await fetch_token_header()
-    microsvc_path = f"{get_settings().PODORC_SERVICE_URL}/po/"
-
-    try:
-        resp_data, status_code = await make_request(
-            url=microsvc_path,
-            method="post",
-            headers=headers,
-            data=props,
-        )
-        logger.info(f"Analysis start response for {analysis_props['analysis_id']}: {resp_data['status']}")
-        return resp_data, status_code
-
-    except HTTPException as e:
-        logger.error(f"Unable to start analysis {analysis_props['analysis_id']} due to the following error: {e}")
-
-
-async def fetch_analysis_status(analysis_id: uuid.UUID | str) -> dict | None:
-    """Fetch the status for a specific analysis run. For headless operation"""
-    headers = await fetch_token_header()
-    microsvc_path = f"{get_settings().PODORC_SERVICE_URL}/po/{analysis_id}/status"
-    resp_data = None
-
-    try:
-        resp_data, _ = await make_request(
-            url=microsvc_path,
-            method="get",
-            headers=headers,
+        node_metadata = get_node_metadata_for_url(analysis_props["node_id"])
+        analysis_info = get_registry_metadata_for_url(node_metadata)
+        props = compile_analysis_pod_data(
+            analysis_id=analysis_props["analysis_id"],
+            project_id=analysis_props["project_id"],
+            compiled_info=analysis_info,
+            kong_token=kong_token,
         )
 
-    except HTTPException as e:
-        logger.error(f"Unable to fetch the status of analysis {analysis_id} due to the following error: {e}")
+        headers = await self.fetch_token_header()
+        microsvc_path = f"{get_settings().PODORC_SERVICE_URL}/po/"
 
-    return resp_data
+        try:
+            resp_data, status_code = await make_request(
+                url=microsvc_path,
+                method="post",
+                headers=headers,
+                data=props,
+            )
+            logger.info(f"Analysis start response for {analysis_props['analysis_id']}: {resp_data['status']}")
+            return resp_data, status_code
 
+        except HTTPException as e:
+            logger.error(f"Unable to start analysis {analysis_props['analysis_id']} due to the following error: {e}")
 
-async def get_valid_projects() -> set:
-    """Collect the available data stores and create a set of project UUIDs with a valid data store."""
-    kong_routes = await list_projects(project_id=None, detailed=False)
-    valid_projects = set()
+    async def fetch_analysis_status(self, analysis_id: uuid.UUID | str) -> dict | None:
+        """Fetch the status for a specific analysis run. For headless operation"""
+        headers = await self.fetch_token_header()
+        microsvc_path = f"{get_settings().PODORC_SERVICE_URL}/po/{analysis_id}/status"
+        resp_data = None
 
-    for route in kong_routes.data:
-        proj_uuid_chunks = route.name.split("-")[:-1]
-        proj_uuid = "-".join(proj_uuid_chunks)
-        valid_projects.add(proj_uuid)
+        try:
+            resp_data, _ = await make_request(
+                url=microsvc_path,
+                method="get",
+                headers=headers,
+            )
 
-    return valid_projects
+        except HTTPException as e:
+            logger.error(f"Unable to fetch the status of analysis {analysis_id} due to the following error: {e}")
 
+        return resp_data
 
-def parse_analyses(analyses: list, valid_projects: set) -> set:
-    """Iterate through analyses and check whether they are approved, built, and have a run status."""
-    ready_analyses = set()
-    for entry in analyses:
-        analysis_id, project_id, node_id, build_status, run_status, approved = (
-            str(entry.analysis_id),
-            str(entry.analysis.project_id),
-            str(entry.node_id),
-            entry.analysis.build_status,
-            entry.run_status,
-            entry.approval_status,
-        )
-        if approved == "approved" and build_status == "finished" and not run_status and project_id in valid_projects:
-            valid_entry = (analysis_id, project_id, node_id, build_status, run_status)
-            ready_analyses.add(valid_entry)
+    async def get_valid_projects(self) -> set:
+        """Collect the available data stores and create a set of project UUIDs with a valid data store."""
+        kong_routes = await list_projects(hub_adapter_settings=self.settings, project_id=None, detailed=False)
+        valid_projects = set()
 
-    logger.info(f"Found {len(ready_analyses)} valid analyses ready to start")
-    return ready_analyses
+        for route in kong_routes.data:
+            proj_uuid_chunks = route.name.split("-")[:-1]
+            proj_uuid = "-".join(proj_uuid_chunks)
+            valid_projects.add(proj_uuid)
+
+        return valid_projects
+
+    @staticmethod
+    def parse_analyses(analyses: list, valid_projects: set) -> set:
+        """Iterate through analyses and check whether they are approved, built, and have a run status."""
+        ready_analyses = set()
+        for entry in analyses:
+            analysis_id, project_id, node_id, build_status, run_status, approved = (
+                str(entry.analysis_id),
+                str(entry.analysis.project_id),
+                str(entry.node_id),
+                entry.analysis.build_status,
+                entry.run_status,
+                entry.approval_status,
+            )
+            if (
+                approved == "approved"
+                and build_status == "finished"
+                and not run_status
+                and project_id in valid_projects
+            ):
+                valid_entry = (analysis_id, project_id, node_id, build_status, run_status)
+                ready_analyses.add(valid_entry)
+
+        logger.info(f"Found {len(ready_analyses)} valid analyses ready to start")
+        return ready_analyses
