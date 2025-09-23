2ac981879d4af1555638e9ef2bd2bc442ab5c14e
Bruce Schultz <bschultz013@gmail.com>
2025-09-23 11:41:04 +0200

    feat(meta): add endpoint for initializing an analysis using headless methods

--- hub_adapter/routers/meta.py
+++ hub_adapter/routers/meta.py
@@ -4,7 +4,8 @@ import logging
 import uuid
 from typing import Annotated
 
-from fastapi import APIRouter, Depends, Form, Security
+import flame_hub
+from fastapi import APIRouter, Depends, Form, HTTPException, Security
 from pydantic import BaseModel
 from starlette import status
 
@@ -13,6 +14,7 @@ from hub_adapter.auth import (
     jwtbearer,
     verify_idp_token,
 )
+from hub_adapter.dependencies import get_core_client
 from hub_adapter.headless import GoGoAnalysis
 from hub_adapter.models.podorc import (
     CreatePodResponse,
@@ -39,15 +41,46 @@ class InitializeAnalysis(BaseModel):
 )
 async def initialize_analysis(
     analysis_params: Annotated[InitializeAnalysis, Form(description="Required information to start analysis")],
+    core_client: Annotated[flame_hub.CoreClient, Depends(get_core_client)],
 ):
     """Perform the required checks to start an analysis and send information to the PO."""
     initiator = GoGoAnalysis()
     node_id, node_type = await initiator.describe_node()
+
+    analysis = core_client.find_analysis_nodes(filter={"analysis_id": analysis_params.analysis_id})
+    if not analysis:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail={
+                "message": f"Analysis {analysis_params.analysis_id} not found",
+                "service": "Hub",
+                "status_code": status.HTTP_404_NOT_FOUND,
+            },
+        )
+
+    valid_projects = await initiator.get_valid_projects()
+    parsed_analyses = initiator.parse_analyses([analysis[0]], valid_projects)
+    ready_to_start_analyses = [analysis[0] for analysis in parsed_analyses]
+
+    if analysis_params.analysis_id not in ready_to_start_analyses:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail={
+                "message": "Analysis not ready",
+                "service": "Hub",
+                "status_code": status.HTTP_404_NOT_FOUND,
+            },
+        )
+
     po_resp, po_status_code = await initiator.register_and_start_analysis(
         node_id=node_id, node_type=node_type, **analysis_params.model_dump()
     )
+
     if po_resp:
         return po_resp
 
     else:
-        {"msg": "Failed to initialize analysis"}
+        raise HTTPException(
+            status_code=po_status_code,
+            detail={"message": "Failed to initialize analysis", "status_code": po_status_code},
+        )
