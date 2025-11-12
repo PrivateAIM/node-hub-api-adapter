45f1252a5df984d3668af2374c11b3ede78e20b9
Bruce Schultz <bschultz013@gmail.com>
2025-11-12 08:52:59 +0100

    fix(results): add query params to delete ep

--- hub_adapter/routers/results.py
+++ hub_adapter/routers/results.py
@@ -24,6 +24,7 @@ results_router = APIRouter(
     path="/local",
     status_code=status.HTTP_200_OK,
     service_url=get_settings().RESULTS_SERVICE_URL,
+    query_params=["project_id"],
 )
 async def delete_local_results(
     project_id: Annotated[uuid.UUID | str, Query(description="UUID of the associated project.")],
