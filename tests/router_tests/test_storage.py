15075e1653e4f93b3224dc5d9772dcd642c02803
Bruce Schultz <bschultz013@gmail.com>
2026-02-10 10:42:28 +0100

    test(storage): test docstring

--- tests/router_tests/test_storage.py
+++ tests/router_tests/test_storage.py
@@ -7,5 +7,5 @@ class TestStorage:
     """Storage service endpoint configuration tests."""
 
     def test_route_configs(self, test_client, mock_event_logger):
-        """Test end point configurations for the Hub gateway routes."""
+        """Test end point configurations for the storage service routes."""
         check_routes(storage_router, EXPECTED_STORAGE_ROUTE_CONFIG, test_client, mock_event_logger)
